d1404d1f2037b45de15489179e22e72cd9851d55
Susheel Sathyaraj <susheel.sathyaraj@gmail.com>
2024-11-15 23:47:02 +0530

    moves all the code inside the main func to separate file for modularity

--- main.go
+++ main.go
@@ -2,41 +2,7 @@ package main
 
 import (
 	"fmt"
-	"net/url"
-	"sync"
 )
-
-type loadBalancer struct {
-	Current int
-	Mutex   sync.Mutex
-}
-
-type Server struct {
-	URL       *url.URL
-	IsHealthy bool
-	Mutex     sync.Mutex
-}
-
-// round robin logic
-func (lb *loadBalancer) getNextServer(servers []*Server) *Server {
-	lb.Mutex.Lock()
-	defer lb.Mutex.Unlock()
-
-	for i := 0; i < len(servers); i++ {
-		idx := lb.Current % len(servers)
-		nextServer := servers[idx]
-		lb.Current++
-
-		nextServer.Mutex.Lock()
-		isHealthy := nextServer.IsHealthy
-		nextServer.Mutex.Unlock()
-
-		if isHealthy {
-			return nextServer
-		}
-	}
-	return nil
-}
 
 func main() {
 	fmt.Println("load balancer")
