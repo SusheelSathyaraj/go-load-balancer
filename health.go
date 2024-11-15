c08f4c710f02eb0698d14c1325327f14cdc3a8ee
Susheel Sathyaraj <susheel.sathyaraj@gmail.com>
2024-11-16 00:04:35 +0530

    updates the healthcheck

--- health.go
+++ health.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net/http"
 	"time"
 )
 
@@ -8,12 +9,18 @@ func HealthCheck(servers []*Server, interval time.Duration) {
 	for {
 		for _, server := range servers {
 			go func(s *Server) {
+				//sending a HTTP GET request to the server’s health endpoint
+				resp, err := http.Get(s.Address + "/health")
 
+				s.Mutex.Lock()
+				defer s.Mutex.Unlock()
+
+				if err != nil || resp.StatusCode != http.StatusOK {
+					s.IsHealthy = false
+				}
+				s.IsHealthy = true
 			}(server)
-
 		}
-
 		time.Sleep(interval)
 	}
-
 }
