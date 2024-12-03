53517f13e8b2da844ca0fa1f15ae5fec0de065a7
Susheel Sathyaraj <susheel.sathyaraj@gmail.com>
2024-12-03 22:25:30 +0530

    updates the server logic with the config file as a yaml as a part of yaml

--- server1/server1.go
+++ server1/server1.go
@@ -3,12 +3,53 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"sync"
+
+	"gopkg.in/yaml.v3"
 )
 
-func main() {
-	fmt.Println("this is server 1")
-	port := "8081"
+// struct for reading yaml config file
+type Config struct {
+	Servers                []Server `yaml:"servers"`
+	HealthCheckInterval    int      `yaml:"health_check_interval"`
+	LoadBalancingAlgorithm string   `yaml:"load_balancing_algorithm"`
+}
 
+// struct for the Server type
+type Server struct {
+	Address   string
+	IsHealthy bool
+	Concount  int
+	Mutex     sync.Mutex
+}
+
+func main() {
+	//parsing the config file
+	config := Config{}
+	configData, err := os.ReadFile("config.yaml")
+	if err != nil {
+		fmt.Printf("Error reading config file: %v", err)
+		return
+	}
+	err = yaml.Unmarshal(configData, &config)
+	if err != nil {
+		fmt.Printf("Error parsing the config file: %v", err)
+		return
+	}
+
+	//initialising the server and the laodbalancer
+	servers := make([]*Server, len(config.Servers))
+	for i, srv := range config.Servers {
+		servers[i] = &Server{
+			Address:   srv.Address,
+			IsHealthy: true,
+		}
+	}
+
+	http.HandleFunc("/simulateLoad", func(w http.ResponseWriter, r *http.Request) {
+
+	})
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Println("server is healthy")
