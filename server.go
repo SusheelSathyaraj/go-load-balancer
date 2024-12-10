ed02e794f69f88a574cccaad39919544117586c4
Susheel Sathyaraj <susheel.sathyaraj@gmail.com>
2024-12-10 20:51:39 +0530

    adds the logic to count the number of active connections present on the server at a given time

--- server.go
+++ server.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
 	"sync"
 )
 
-// Backend server
+// Backend server struct
 type Server struct {
 	Address   string
 	IsHealthy bool
 	Mutex     sync.Mutex
 	ConCount  int //for least connection algo
+}
+
+// Active connections count
+
+func (s *Server) activeConnectionsHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+
+		s.Mutex.Lock()
+		defer s.Mutex.Unlock()
+
+		log.Printf("The number of active connections on port %s are %d", s.Address, s.ConCount)
+		fmt.Fprintf(w, "The number of active connections on port %s are %d", s.Address, s.ConCount)
+	}
 }
