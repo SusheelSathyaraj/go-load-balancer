46d9604ec8443f2a913bb913fca7aee66211b7d6
Susheel Sathyaraj <susheel.sathyaraj@gmail.com>
2024-12-12 12:15:18 +0530

    restructing the server.go file to accomodate the func in server.go

---
+++ shared/server.go
@@ -0,0 +1,35 @@
+package shared
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+// Backend server struct
+type Server struct {
+	Address   string
+	IsHealthy bool
+	Mutex     sync.Mutex
+	ConCount  int //for least connection algo
+}
+
+// Active connections count
+
+func (s *Server) ActiveConnectionsHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+
+		s.Mutex.Lock()
+		defer s.Mutex.Unlock()
+
+		healthStatus := "unhealthy"
+		if s.IsHealthy {
+			healthStatus = "healthy"
+		}
+
+		log.Printf("The number of active connections on port %s are %d and the server is %s", s.Address, s.ConCount, healthStatus)
+		fmt.Fprintf(w, "The number of active connections on port %s are %d and the server is %s", s.Address, s.ConCount, healthStatus)
+	}
+}
