2c3a762cdc1f7c5e7f80e374e84c39e2c01f9879
Susheel Sathyaraj <susheel.sathyaraj@gmail.com>
2025-08-14 22:51:32 +0530

    restructuring server code

--- server1/server1.go
+++ server1/server1.go
@@ -2,25 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
-	fmt.Println("this is server1")
+	fmt.Println("Starting server1...")
 
 	port := "8081"
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, "Server is healthy")
+		fmt.Fprintf(w, "Server1 is healthy")
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("Server is running on port: %s", port)
+		//Simulate some processing time
+		time.Sleep(100 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Response from Server1 (port %s) at %s", port, time.Now().Format("15:04:05"))
+		log.Printf("Handled request on Server1")
 	})
 
-	fmt.Printf("Starting the server on port: %s\n", port)
+	log.Printf("Starting Server 1 on port: %s", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		fmt.Printf("Server is not running on port %s, error: %v", port, err)
+		log.Fatalf("Server 1 failed to start on port: %s,%v", port, err)
 	}
 }
