0d82d2b3af6b234b24bdc5bcc45d6e91ef765472
Susheel Sathyaraj <susheel.sathyaraj@gmail.com>
2025-08-14 23:44:41 +0530

    refactors the health.go

--- health.go
+++ health.go
@@ -1,26 +1,65 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"sync"
 	"time"
 )
 
-func HealthCheck(servers []*Server, interval time.Duration) {
+func HealthCheck(servers []*Server, interval time.Duration, ctx context.Context) {
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	//Initial Health Checks
+	checkAllServers(servers)
+
 	for {
-		for _, server := range servers {
-			go func(s *Server) {
-				//sending a HTTP GET request to the server’s health endpoint
-				resp, err := http.Get(s.Address + "/health")
-
-				s.Mutex.Lock()
-				defer s.Mutex.Unlock()
-
-				if err != nil || resp.StatusCode != http.StatusOK {
-					s.IsHealthy = false
-				}
-				s.IsHealthy = true
-			}(server)
+		select {
+		case <-ctx.Done():
+			log.Println("Stopping health checks")
+			return
+		case <-ticker.C:
+			checkAllServers(servers)
 		}
-		time.Sleep(interval)
+	}
+}
+
+func checkAllServers(servers []*Server) {
+	var wg sync.WaitGroup
+	for _, server := range servers {
+		wg.Add(1)
+		go func(s *Server) {
+			defer wg.Done()
+			checkserverHealth(s)
+		}(server)
+	}
+	wg.Wait()
+}
+
+func checkserverHealth(server *Server) {
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	resp, err := client.Get(server.Address + "/health")
+
+	server.Mutex.RLock()
+	defer server.Mutex.RUnlock()
+
+	previousHealth := server.IsHealthy
+
+	if err != nil || resp.StatusCode != http.StatusOK {
+		server.IsHealthy = false
+		if previousHealth {
+			log.Printf("Server %s is unhealthy ,%v", server.Address, err)
+		}
+	} else {
+		server.IsHealthy = true
+		if !previousHealth {
+			log.Printf("Server %s is healthy", server.Address)
+		}
+		resp.Body.Close()
 	}
 }
