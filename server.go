73232e2fb37ae175d892e000f171310fdd9d4cbf
Susheel Sathyaraj <susheel.sathyaraj@gmail.com>
2025-08-17 21:58:41 +0530

    adds support methods for server.go

--- server.go
+++ server.go
@@ -12,7 +12,7 @@ import (
 type Server struct {
 	Address   string
 	IsHealthy bool
-	Mutex     sync.Mutex
+	Mutex     sync.RWMutex
 	ConCount  int //for least connection algo
 	URL       *url.URL
 }
@@ -33,7 +33,54 @@ func NewServer(address string) (*Server, error) {
 	}, nil
 }
 
-// Active connections count
+// getting current connection count
+func (s *Server) GetConnectionCount() int {
+	s.Mutex.RLock()
+	defer s.Mutex.RUnlock()
+	return s.ConCount
+}
+
+// Increments the connection count
+func (s *Server) IncrementConnectionCount() {
+	s.Mutex.Lock()
+	defer s.Mutex.Unlock()
+	s.ConCount++
+}
+
+// Decrements the connection count
+func (s *Server) DecrementConnectionCount() {
+	s.Mutex.Lock()
+	defer s.Mutex.Unlock()
+	if s.ConCount > 0 {
+		s.ConCount--
+	}
+}
+
+// Returning if the server is healthy (thread safe)
+func (s *Server) IsServerHealthy() bool {
+	s.Mutex.RLock()
+	defer s.Mutex.RUnlock()
+	return s.IsHealthy
+}
+
+// setting the health status (thread safe)
+func (s *Server) SetHealthy(healthy bool) {
+	s.Mutex.Lock()
+	defer s.Mutex.Unlock()
+	s.IsHealthy = healthy
+}
+
+// returning server info as a map
+func (s *Server) GetServerInfo() map[string]interface{} {
+	s.Mutex.RLock()
+	defer s.Mutex.RUnlock()
+
+	return map[string]interface{}{
+		"address":      s.Address,
+		"healthy":      s.IsHealthy,
+		"connnections": s.ConCount,
+	}
+}
 
 func (s *Server) ActiveConnectionsHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
